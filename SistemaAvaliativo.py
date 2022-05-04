9138f9bdcf7848c77b8d0ec7c071a6b3bf6cbf57
Lucas <lucaas_batista@hotmail.com>
2022-05-03 21:39:46 -0300

    Desenvolvimento das perguntas

--- SistemaAvaliativo.py
+++ SistemaAvaliativo.py
@@ -1,18 +1,40 @@
 nome = str(input('Digite o nome do funcionário: '))
-for c in range(0, 4):
+for c in range(0, 6):
     print('''Aqui você escolhe sobre qual assunto deseja tratar a avaliação:
-    [ 1 ] Objetivos 
-    [ 2 ] Performance
-    [ 3 ] Competências
-    [ 4 ] Evolução''')
+    [ 1 ] Performance 
+    [ 2 ] Valores Da Empresa
+    [ 3 ] Treinamento
+    [ 4 ] Escelência Técnica e Funcional
+    [ 5 ] Trabalho em Equipe
+    [ 6 ] Entrega e Resultados''')
     print('Nota: 0 á 10')
     escolha = int(input('O que deseja avaliar: '))
     if escolha == 1:
-        print('Os objetivos do funcionário está de acordo com os objetivos que a empresa propoe à ele?')
-        nota1 = float(input('Nota: '))
-        print('O funcionário concorda com a proposta que a empresa fornece à ele?')
-    #if escolha == 2:
-
-    #if escolha == 3:
-
-    #if escolha == 4:
+        print('O funcionários executa seu trabalho atendendo aos padrões da empresa?')
+        n1 = int(input('Nota: '))
+        print('Contribui com sugestões de melhoria dos processos e serviços da empresa?')
+        n2 = int(input('Nota: '))
+    if escolha == 2:
+        print('Demostra os valores da empresa no dia a dia de trabalho?')
+        n3 = int(input('Nota: '))
+        print('Participa das iniciativas que a empresa oferece?')
+        n4 = int(input('Nota: '))
+    if escolha == 3:
+        print('Demostra os conhecimentos necessários para atuar na área?')
+        n5 = int(input('Nota: '))
+        print('Participa efetivamente das ações de treinamento e conscientização de sua área?')
+        n6 = int(input('Nota: '))
+    if escolha == 4:
+        print('Demostra conhecimento, analisa criticamente o processo e busca aperfeiçoamento técnico?')
+        n7 = int(input('Nota: '))
+    if escolha == 5:
+        print('Respita, ouve, ajuda e valoriza os colegas de trabalho?')
+        n8 = int(input('Nota: '))
+        print('Comunica-se de maneira clara, concisa e trasnparente?')
+        n9 = int(input('Nota: '))
+    if escolha == 6:
+        print('Cumpre os objetivos estabelecidos para si e suporta os colegas para o atingimentos dos objetivos?')
+        n10 = int(input('Nota: '))
+        print('Dentre de seu escopo de trabalho, toma decisões adequadas seguindo os procedimentos, dados e fatos?')
+        n11 = int(input('Nota: '))
+print('Fim do programa')
